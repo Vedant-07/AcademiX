1fdbd51431735743ae6db0cffe1e67535a3c0ebc
Vedant-07 <vedantnpatel7@gmail.com>
2025-04-09 09:05:48 +0530

    added the cors for connecting it to frontend

--- src/app.js
+++ src/app.js
@@ -7,12 +7,20 @@ const cookieParser = require("cookie-parser");
 const postRouter = require("./routes/postRouter");
 const commentRouter = require("./routes/commentRouter");
 const voteRouter = require("./routes/voteRouter");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 7777;
 
 const app = express();
 app.use("/", express.json());
 app.use(cookieParser());
+// Enable CORS with credentials
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 app.use("/auth", authRouter);
 app.use("/profile", profileRouter);
